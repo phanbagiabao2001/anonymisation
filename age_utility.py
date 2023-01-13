aa898f639048bf91ba5e090056b780204cd5afe2
Phan Ba Gia Bao <phanbagiabao2001@gmail.com>
2023-01-13 15:36:22 +0100

    Add files utility

---
+++ age_utility.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('dataset.csv',sep = ";" )
+
+df_2 = df.drop(['name', 'team','nationality','position'],1)
+
+df_origin = df_2.head(10)
+
+df ={'player_id':['169134','31912','190871', '203376','200389', '192985', '192985','188545', '183277', '212831']
+        , 'age':['29','45', '29','31','40','34','35','36','24','29' ]
+        , 'hits':['150','289', '269','137','50','120','90','70','40','100']
+    }
+
+df_anonym = pd.DataFrame(df)
+
+def main(df_origin, df_anonym ):
+
+    df_origin = df_origin.copy()
+    df_anonym = df_anonym.copy()
+
+    df_origin = df_origin.astype({'age': 'int64', 'hits': 'int64'})
+    df_anonym = df_anonym.astype({'age': 'int64', 'hits': 'int64'})
+
+    df_origin['score'] = abs(df_origin['age'] - df_anonym['age'])/df_anonym['age']
+    
+    print(df_origin['score'].mean())
+    
+if __name__ == "__main__":
+    main(df_origin,df_anonym)
